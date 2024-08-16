cf611a85bd927c3d59bdb90e12f9a1391cce8977
Zeeshan Javaid <902032fe@gmail.com>
2024-08-16 12:27:18 +0500

    some changes

--- ExpenseManagement/index.js
+++
@@ -1,6 +0,0 @@
-import { AppRegistry } from 'react-native';
-import App from './App';
-import { name as appName } from './app.json';
-
-// Register the root component for the app
-AppRegistry.registerComponent(appName, () => App);
--- ExpenseManagement/metro.config.js
+++
@@ -1,8 +0,0 @@
-// metro.config.js
-module.exports = {
-    resolver: {
-      assetExts: ['db', 'mp4', 'svg'],
-      sourceExts: ['jsx', 'js', 'ts', 'tsx'],
-    },
-  };
-  
